cde7cd256244b597c328c687244b36a07f3ac774
Dream-World-Coder <dreamy.star.368@gmail.com>
2026-06-10 03:32:10 +0530

    feat: added github workflow

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
+    output: "export",
     images: {
+        images: { unoptimized: true },
         remotePatterns: [
             new URL("https://picsum.photos/**"),
             new URL("https://i.imgur.com/**"),
